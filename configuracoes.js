234338692e696e1d7fe27b667f94db4f0392b966
Dario E. de Araújo <112591227+Dario-Evangelista@users.noreply.github.com>
2023-06-15 22:01:36 -0300

    Update configuracoes.js
    update

--- configuracoes.js
+++ configuracoes.js
@@ -1,53 +1,78 @@
 function cadastrarUsuario() {
-    var usuario = document.getElementById("usuario").value;
-    var cadastrarSenha = document.getElementById("cadastrarSenha").value;
+    var alterarusuario = document.getElementById("alterarusuario").value;
+    var nomeEmpresa = document.getElementById("nomeEmpresa").value;
+    var mudarSenha = document.getElementById("mudarSenha").value;
     var email = document.getElementById("email").value;
+    var alterarnome = document.getElementById("alterarnome").value;
 
-    localStorage.setItem(usuario + "cadastrarSenha", cadastrarSenha);
-    localStorage.setItem(usuario + "email", email);
+    localStorage.setItem(alterarusuario + "mudarSenha", mudarSenha);
+    localStorage.setItem(alterarusuario + "nomeEmpresa", nomeEmpresa);
+    localStorage.setItem(alterarusuario + "alterarnome", alterarnome);
+    localStorage.setItem(alterarusuario + "email", email);
 
     alert("Usuário cadastrado com sucesso!");
     document.getElementById("mudarSenha").value = "";
     document.getElementById("email").value = "";
+    document.getElementById("alterarusuario").value = "";
+    document.getElementById("alterarnome").value = "";
+    document.getElementById("nomeEmpresa").value = "";
 }
 
-function carregarUsuario() {
-    var usuarioCarregar = document.getElementById("usuarioCarregar").value;
-    var cadastrarSenha = localStorage.getItem(usuarioCarregar + "cadastrarSenha");
-    var email = localStorage.getItem(usuarioCarregar + "email");
+document.getElementById("alterarusuario").addEventListener('blur', function (e)  {
+    var alterarusuario = document.getElementById("alterarusuario").value;
+    var mudarSenha = localStorage.getItem(alterarusuario + "mudarSenha");
+    var email = localStorage.getItem(alterarusuario + "email");
+    var nomeEmpresa = localStorage.getItem(alterarusuario + "nomeEmpresa");
+    var alterarnome = localStorage.getItem(alterarusuario + "alterarnome");
 
-    if (cadastrarSenha === null || email === null) {
+    if (mudarSenha === null || email === null) {
         alert("Usuário não encontrado.");
     } else {
-        document.getElementById("mudarSenha").value = cadastrarSenha;
+        document.getElementById("mudarSenha").value = mudarSenha;
         document.getElementById("email").value = email;
+        document.getElementById("alterarnome").value = alterarnome;
+        document.getElementById("nomeEmpresa").value = nomeEmpresa;
     }
-}
+
+
+document.getElementById("alterarusuario").addEventListener('focus', function (e) {
+document.getElementById("alterarusuario").removeEventListener('blur', carregarDados);
+})});
 
 function alterarNomeUsuario() {
-    var usuarioCarregar = document.getElementById("usuarioCarregar").value;
-    var novoUsuario = document.getElementById("alterarnome").value;
+    var alterarusuario = document.getElementById("alterarusuario").value;
+    var nomeEmpresa = document.getElementById("nomeEmpresa").value;
+    var mudarSenha = document.getElementById("mudarSenha").value;
+    var email = document.getElementById("email").value;
+    var alterarnome = document.getElementById("alterarnome").value;
 
-    var cadastrarSenha = localStorage.getItem(usuarioCarregar + "cadastrarSenha");
-    var email = localStorage.getItem(usuarioCarregar + "email");
+    var mudarSenhaAnterior = localStorage.getItem(alterarusuario + "mudarSenha");
+    var nomeEmpresaAnterior = localStorage.getItem(alterarusuario + "nomeEmpresa");
+    var alterarnomeAnterior = localStorage.getItem(alterarusuario + "alterarnome");
+    var emailAnterior = localStorage.getItem(alterarusuario + "email");
 
-    if (cadastrarSenha === null || email === null) {
+    if (mudarSenhaAnterior === null || emailAnterior === null) {
         alert("Usuário não encontrado.");
     } else {
-        localStorage.removeItem(usuarioCarregar + "cadastrarSenha");
-        localStorage.removeItem(usuarioCarregar + "email");
+        localStorage.removeItem(alterarusuario + "mudarSenha");
+        localStorage.removeItem(alterarusuario + "email");
+        localStorage.removeItem(alterarusuario + "alterarnome");
+        localStorage.removeItem(alterarusuario + "nomeEmpresa");
 
-        localStorage.setItem(novoUsuario + "cadastrarSenha", cadastrarSenha);
-        localStorage.setItem(novoUsuario + "email", email);
+        localStorage.setItem(alterarusuario + "mudarSenha", mudarSenha);
+        localStorage.setItem(alterarusuario + "email", email);
+        localStorage.setItem(alterarusuario + "alterarnome", alterarnome);
+        localStorage.setItem(alterarusuario + "nomeEmpresa", nomeEmpresa);
 
         alert("Nome de usuário alterado com sucesso!");
         document.getElementById("alterarnome").value = "";
         document.getElementById("mudarSenha").value = "";
         document.getElementById("email").value = "";
-        document.getElementById("cadastrarSenha").value = "";
-        document.getElementById("usuario").value = "";
+        document.getElementById("alterarusuario").value = "";
+        document.getElementById("nomeEmpresa").value = "";
     }
 }
+
 
 
 
